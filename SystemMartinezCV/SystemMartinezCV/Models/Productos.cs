f9d4a74cdf0c464e3b2cc0aa52dba7a708c983d6
Mario Alvarenga <Mario Alvarenga@DESKTOP-B93F6A7>
2020-01-23 10:16:26 -0600

    productos model

---
+++ SystemMartinezCV/SystemMartinezCV/Models/Productos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SystemMartinezCV.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string EstadoEliminar { get; set; }
+
+        public int IdUnidadMedida { get; set; }
+        public virtual UnidadMedida UnidadMedida { get; set; }
+
+        public int IdCategoria { get; set; }
+        public virtual Categorias Categorias { get; set; }
+    }
+}
--- SystemMartinezCV/SystemMartinezCV/Models/UnidadMedida.cs
+++ SystemMartinezCV/SystemMartinezCV/Models/UnidadMedida.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace SystemMartinezCV.Models
 {
-    public class Categorias
+    public class UnidadMedida
     {
-        [Key]
-        public int IdCategoria { get; set; }
-        public string Categoria { get; set; }
+        public int IdUnidadMedida { get; set; }
+        public string Unidad { get; set; }
         public string Descripcion { get; set; }
 
         public virtual List<Productos> Productos { get; set; }
