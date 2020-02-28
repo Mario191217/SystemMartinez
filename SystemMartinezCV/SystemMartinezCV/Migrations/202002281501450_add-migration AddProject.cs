745ab887ee5d9e4f38c3240815c0d88bbdcf66aa
ulices.argueta <ulices.argueta@CDS1UNIVO37>
2020-02-28 10:10:00 -0600

    ptm x2

---
+++ SystemMartinezCV/SystemMartinezCV/Migrations/202002281501450_add-migration AddProject.cs
@@ -0,0 +1,18 @@
+﻿namespace SystemMartinezCV.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addmigrationAddProject : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Proyectos", "Costo", c => c.Double(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Proyectos", "Costo");
+        }
+    }
+}
